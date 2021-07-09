6ef60c44391c62e9a2ee2186ef7f9bd5928c7cf2
Guilherme Martins <g.santosmartins@outlook.com>
2021-07-09 14:16:03 -0300

    Tratativa de nota vazia

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './estilo.css';
+import React, { Component } from "react";
 
-export default function CardNota() {
-  return (
-    <section className="card-nota">
-      <header className="card-nota_cabecalho">
-        <h3 className="card-nota_titulo">Título</h3>
-      </header>
-      <p className="card-nota_texto">Escreva sua nota</p>
-    </section>
-  );
+import deleteSVG from '../../assets/img/delete.svg'
+
+import "./estilo.css";
+class CardNota extends Component {
+  render() {
+    return (
+      <section className="card-nota">
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img src={deleteSVG} />
+        </header>
+        <p className="card-nota_texto">{this.props.texto}</p>
+      </section>
+    );
+  }
 }
+
+export default CardNota;
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import CardNota from '../CardNota/CardNota';
-import './estilo.css';
-
-export default function ListaDeNotas() {
-  return (
-
-    <ul className="lista-notas">
-      {Array.of('Trabalho', 'Trabalho', 'Estudos').map(() => (
-        <li className="lista-notas_item">
-          <CardNota />
-        </li>
-      ))}
-    </ul>
-  );
+import React, { Component } from "react";
+import CardNota from "../CardNota";
+import "./estilo.css";
+class ListaDeNotas extends Component {
+ 
+  render() {
+    return (
+      <ul className="lista-notas">
+        {this.props.notas.map((nota, index) => {
+          return (
+            <li className="lista-notas_item" key={index}>
+              <CardNota titulo={nota.titulo} texto={nota.texto}/>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
+
+export default ListaDeNotas;
